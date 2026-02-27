1ef071b9eb3299bbc83d45f46cec48faeede68d0
muktar01k <adekanyeadewale7@gmail.com>
2026-02-27 18:52:47 +0100

    scroll to the top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Nav } from './Nav';
 import { Footer } from './Footer';
+import { ScrollToTop } from './ScrollToTop';
 
 export function Layout() {
   return (
     <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden">
+      <ScrollToTop />
       <Nav />
       <main className="flex-1 pt-10">
         <Outlet />
